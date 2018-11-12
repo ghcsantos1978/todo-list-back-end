f04b57cbd3921bb71b233ab911e1073c8471d0b4
ghcsantos1978 <ghcsantos1978@gmail.com>
2018-11-12 13:42:36 -0200

    servicos

--- src/main/java/br/sonda/todolist/controller/ToDoListController.java
+++ src/main/java/br/sonda/todolist/controller/ToDoListController.java
@@ -12,6 +12,7 @@ import javax.validation.constraints.NotNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +26,7 @@ import br.sonda.todolist.service.ToDoService;
 
 @RestController
 @RequestMapping("/todoList")
+@CrossOrigin("*")
 public class ToDoListController {
 
 	@Autowired
